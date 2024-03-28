6e4a1f398b0319c8525ccb24ee09c009ec567caf
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2024-03-28 12:06:39 +0900

    [Bronze V] Title: A+B - 7, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,7 +1,5 @@
-n= int(input())
+T = int(input())
 
-for i in range(n):
-    x,y= input().split()
-    x = int(x)
-    y = int(y)
-    print("Case #"+str(i+1)+": "+str(x+y))
+for i in range(T):
+    a,b = map(int, input().split())
+    print("Case #",i+1,": ",a+b,sep='')
