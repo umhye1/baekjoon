e31919e6c4fad00cd6bf19895d8182b354bc4474
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2024-04-12 17:43:26 +0900

    [Bronze III] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,42 @@
+s = str(input())
+
+if s =="A+":
+    score =4.3
+
+elif s =="A0":
+    score =4.0
+
+elif s =="A-":
+    score =3.7
+
+elif s =="B+":
+    score =3.3
+
+elif s =="B0":
+    score =3.0
+
+elif s =="B-":
+    score =2.7
+
+elif s =="C+":
+    score =2.3
+
+elif s =="C0":
+    score =2.0
+
+elif s =="C-":
+    score =1.7
+
+elif s =="D+":
+    score =1.3
+
+elif s =="D0":
+    score =1.0
+
+elif s =="D-":
+    score =0.7
+
+elif s =="F":
+    score =0.0
+
+print(score)
