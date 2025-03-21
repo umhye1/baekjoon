f146d12aa9fc19fe27632c33304c09990f09c6b3
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2025-03-21 23:20:46 +0900

    [Bronze II] Title: 수 정렬하기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,11 +1,13 @@
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
 n = int(input())
-x=list()
+a=[]
 
 for i in range(n):
-    m= int(input())
-    x.append(m)
+    n =int(input())
+    a.append(n)
 
-x.sort()
+a.sort()
 
-for i in range(n):
-    print(x[i])
+for i in range(len(a)):
+    print(a[i])
