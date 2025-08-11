5e33cc355cc0cc4fce2c41aab1fb329759184db0
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2025-08-11 16:31:05 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 35700 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,76 +1,58 @@
-# n = 정점의 개수
-# m = 간선의 개수
-# v = 시작 정점
+import sys
+from collections import deque
+# n : 정점의 개수
+# m : 간선 개수
+# v : 시작 정점
 
 # dfs
 # 1. 탐색 시작 노드를 스택에 삽입하고 방문 처리를 함
 # 2. 스택의 최상단 노드에 방문하지 않은 인접한 노드가 하나라도 있다면 그 노드를 스택에 넣고 방문 처리함
 #   방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드를 꺼냄
+# g = graph
+# v = 방문할 노드(시작노드)
+
+n,m,v = map(int,sys.stdin.readline().split())
+
+graph = [[] for _ in range(n+1)] # 연결된 모든 정점 확인용 그래프
+visited = [False for _ in range(n+1)] # 방문 확인용 그래프
+visited[0] = True
+
+for i in range(m):
+    x,y = map(int, sys.stdin.readline().split())
+    graph[x].append(y)
+    graph[y].append(x)
+    graph[x].sort()
+    graph[y].sort()
+
+
+def dfs(graph,v,visited):
+    visited[v] = True
+    print(v,end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+dfs(graph,v,visited)
+print("")
 
 # bfs
 # 1. 탐색 시작 노드를 큐에 삽입하고 방문처리를 함
 # 2. 큐에서 노드를 꺼낸 뒤 해당 노드의 인접 노드 중에서 방문하지 않은 노드를 모두 큐에 삽입하고 방문처리함
 
-from collections import deque
+visited = [False for _ in range(n+1)] # 방문 확인용 그래프
+visited[0] = True
 
-n,m,v = map(int, input().split())
-graph =[[] for _ in range(n+1)]
+def bfs(graph, v, visited):
+    queue = deque([v])
+    visited[v] = True
 
-for i in range(m) :
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-    graph[x].sort()
-    graph[y].sort()
-    
-
-def dfs(x) :
-    if x <0 or x>=n+1 :
-        return False
-    
-    visited[x] = True
-    print(x,end =" ")
-
-    for i in graph[x]:
-        if visited[i] == False :
-            dfs(i)
-
-def bfs(x) :
-    if x <0 or x>=n+1 :
-        return False
-    
-    queue = deque([x])
-    visited[x] = True
-
-    while queue :
-        
-        front = queue.popleft()
-        print(front, end=" ")
-        
-
-        for i in graph[front] :
-            if visited[i] == False:
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
                 queue.append(i)
-                visited[i] =True
-                
-    
-visited = [False] * (n+1) 
-dfs(v)
-print("")
+                visited[i] = True
+  
 
-visited = [False] * (n+1) 
-bfs(v)
-# print(bfs(v))
-
-# 1 : 2,3,4
-# 2 : 1,4
-# 3 : 1,4
-# 4 : 1,2,3
-
-
-# 1 : 2,3 
-# 2 : 1,5
-# 3 : 1,4
-# 4 : 3,5
-# 5 : 2,4
-
+bfs(graph, v, visited)
