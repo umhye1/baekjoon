1c73ca51866853373b2a3d21e32d27e17edd89a8
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2025-08-19 15:54:00 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 92 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+while True:
+    stack = deque()
+    a = sys.stdin.readline().rstrip()
+
+    if a == '.':
+        break
+        
+
+    for i in a:
+        if i == "[" or i == "(":
+            stack.append(i)
+
+        elif i == "]":
+            if len(stack) != 0 and stack[-1] == '[' :
+                stack.pop()
+            
+            else :
+                stack.append("]")
+                break
+        
+        elif i == ")":
+            if len(stack) != 0 and stack[-1] == '(' :
+                stack.pop()
+            
+            else :
+                stack.append(")")
+                break
+        
+    if len(stack) == 0 :
+        print("yes")
+    else :
+        print("no")
