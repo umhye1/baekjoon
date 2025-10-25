adeb6ec2fdfe2267ed912851ef3bf057e54d2d55
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2025-10-25 10:18:11 +0900

    [level 1] Title: 잔챙이 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 작성해주세요
+# 잡은 물고기 중 길이가 10cm 이하인 물고기의 수를 출력하는 SQL 문을 작성해주세요.
+# 물고기의 수를 나타내는 컬럼 명은 FISH_COUNT
+select count(*) as fish_count
+from fish_info
+where length <= 10 or length is null;
