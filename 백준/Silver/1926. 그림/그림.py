e723598ce02c054bda4dbd2667733fed98ad94bd
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2026-01-16 21:42:11 +0900

    [Silver I] Title: 그림, Time: 292 ms, Memory: 35828 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,48 +1,48 @@
-import sys
 from collections import deque
 
-n,m = map(int,sys.stdin.readline().split()) # 세로, 가로
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
-graph = [[] for _ in range(n)]
-visited = [[False]*m for _ in range(n)]
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-cnt = []
+graph = []
 
-def bfs(graph,y,x): # 세로 가로
+count = 0
+grim = []
+
+n,m = map(int, input().split()) # 세로, 가로
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+def bfs(a,b):
     queue = deque()
-    queue.append((y,x))
-    visited[y][x] = True
-    count = 1
+    queue.append((a,b))
+    graph[a][b] = 0
+    g = 1
 
     while queue:
-        cy,cx = queue.popleft()
-
+        x,y = queue.popleft() # 다음에 방문할 x,y좌표
         for i in range(4):
-            ny = dy[i] + cy
-            nx = dx[i] + cx
+            nx = dx[i] + x
+            ny = dy[i] + y
 
-            if 0 <= ny < n and 0 <= nx < m:
-                if graph[ny][nx] == 1 and not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    queue.append((ny,nx))
-                    count += 1
+            if 0<=nx<n and 0<=ny<m and  graph[nx][ny] == 1:
+                g += 1
+                graph[nx][ny] = 0
+                queue.append((nx,ny))
 
-    return count
-            
+    return g # 넓이
 
 for i in range(n):
-    graph[i] = list(map(int, sys.stdin.readline().split()))
+    for j in range(m):
+        if graph[i][j] == 1 :
+            grim.append(bfs(i,j))
+            count += 1
 
-for j in range(n):
-    for i in range(m):
-        if graph[j][i] ==1 and  not visited[j][i] :
-            cnt.append(bfs(graph,j,i))
-            
-    
-if  not cnt:
+if count == 0:
     print(0)
     print(0)
 else :
-    print(len(cnt))
-    print(max(cnt))
+    print(count)
+    print(max(grim))
+
+
+
