ac750591e95a30151ce256d5e81178ec921dfa37
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2026-02-27 18:16:33 +0900

    [level 1] Title: 행렬의 덧셈, Time: 32.37 ms, Memory: 22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,9 @@
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        row = []
+        for j in range(len(arr1[i])):
+            row.append(arr2[i][j] + arr1[i][j])
+        answer.append(row)
+        
+    return answer
