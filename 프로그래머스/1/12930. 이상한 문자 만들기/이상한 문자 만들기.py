a29b78747826fe7f83935ff34b0d6e4f7ca3c798
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2026-04-16 17:44:28 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    index = 0
+    for i in s :
+        if i == " ":
+            answer += " "
+            index = 0
+        
+        else :
+            if index % 2 == 0 :
+                answer += i.upper()
+            else :
+                answer += i.lower()
+                
+            index += 1
+    return answer
