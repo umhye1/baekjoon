d25aa2a3e2ef1a0c07d34bedd9be810ec8e06b22
Hyewon Um <127825859+umhye1@users.noreply.github.com>
2026-06-22 13:25:29 +0900

    [level 2] Title: 가장 큰 수, Time: 30.38 ms, Memory: 26.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,15 @@
+# 문자열 특성 이용하기 - 30과 3비교 위해 문자열*3정도해보기
 def solution(numbers):
-    str_numbers = list(map(str, numbers))
-    str_numbers.sort(key = lambda x : x*10, reverse = True)
-    answer = "".join(str_numbers)
+    answer = ''
     
+    numbers = list(map(str, numbers))
+    
+    numbers.sort(key = lambda x : x*3, reverse=True)
+    answer = "".join(numbers)
+
+
     if answer[0] == "0":
         return "0"
+    
     return answer
+        
